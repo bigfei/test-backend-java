92c8ba224d2ed28d6f0bd774c502bc1785d01f33
Felix Xu <xuchenfei@gmail.com>
2023-05-22 08:06:46 +0800

    feat(api): add jacoco for test coverage and more test for UserService

--- src/test/java/com/wiredcraft/wcapi/service/UserServiceTest.java
+++ src/test/java/com/wiredcraft/wcapi/service/UserServiceTest.java
@@ -8,17 +8,21 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
@@ -48,17 +52,17 @@ public class UserServiceTest {
 
     @Test
     void shouldThrowErrorWhenSaveUserWithExistingName() {
-        final User user = new User("Tom", LocalDate.now(),"ADDR1","T1");
+        final User user = new User("Tom", LocalDate.now(), "ADDR1", "T1");
         given(userRepository.findByName(user.getName())).willReturn(Optional.of(user));
-        assertThrows(UserRegistrationException.class,() -> {
+        assertThrows(UserRegistrationException.class, () -> {
             userService.createUser(user);
         });
         verify(userRepository, never()).save(any(User.class));
     }
 
     @Test
-    void updateUser() {
-        final User user = new User("Tom", LocalDate.now(),"ADDR1","T1");
+    void shouldUpdateUser() {
+        final User user = new User("Tom", LocalDate.now(), "ADDR1", "T1");
         user.setId("111a");
         given(userRepository.findById(user.getId())).willReturn(Optional.of(user));
         given(userRepository.save(user)).willReturn(user);
@@ -66,4 +70,46 @@ public class UserServiceTest {
         assertThat(expected).isNotNull();
         verify(userRepository).save(any(User.class));
     }
+
+
+    @Test
+    void shouldReturnFindAll() {
+        List<User> users = new ArrayList();
+        users.add(new User("Tom", LocalDate.now(), "ADDR1", "T1"));
+        users.add(new User("James", LocalDate.now(), "ADDR2", "T2"));
+        users.add(new User("Lisa", LocalDate.now(), "ADDR3", "T3"));
+
+        Pageable paging = PageRequest.of(0, 10);
+        Page<User> expected = new PageImpl(users);
+
+        given(userRepository.findAll(paging)).willReturn(expected);
+        Page<User> actual = userService.getAllUsers(paging);
+
+        assertEquals(3, expected.getTotalElements());
+        assertEquals(users, expected.getContent());
+    }
+
+    @Test
+    void findUserById() {
+        String id = "1a";
+        final User user = new User("Tom", LocalDate.now(), "ADDR1", "T1");
+
+        given(userRepository.findById(id)).willReturn(Optional.of(user));
+
+        Optional<User> expected = userService.getUserById(id);
+
+        assertThat(expected).isNotNull();
+        assertEquals("Tom", expected.get().getName());
+    }
+
+    @Test
+    void shouldBeDelete() {
+        final String userId = "1a";
+
+        userService.deleteUser(userId);
+        userService.deleteUser(userId);
+
+        verify(userRepository, times(2)).deleteById(userId);
+    }
+
 }
